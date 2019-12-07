0bc7796ad69cc98acd58bc1b69c99ea6d56237b7
Peng-Xu <32625993+Peng-Xu@users.noreply.github.com>
2019-12-07 14:00:13 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,8 +1,7 @@
 export GITACCOUNT= Peng-Xu
 
 export MYCLUSTER=tektonknativebeijing28
-export KUBECONFIG=
-
+export KUBECONFIG=/tmp/ic/AB/.bluemix/plugins/container-service/clusters/tektonknativebeijing28/kube-config-tok02-tektonknativebeijing28.yml
 export NAMESPACE=tektondevops-ims-617
 export EMAIL= 969308810@qq.com
-export INGRESS=
+export INGRESS=tektonknativebeijing28-0e3e0ef4c9c6d831e8aa6fe01f33bfc4-0001.jp-tok.containers.appdomain.cloud 
