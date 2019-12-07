f2aceaf1aa374ce803d03fd4f15e53a03ddfd8d6
Peng-Xu <32625993+Peng-Xu@users.noreply.github.com>
2019-12-07 13:42:28 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -3,6 +3,6 @@ export GITACCOUNT= Peng-Xu
 export MYCLUSTER=tektonknativebeijing66
 export KUBECONFIG=
 
-export NAMESPACE=
+export NAMESPACE=tektondevops-ims-617
 export EMAIL= 969308810@qq.com
 export INGRESS=
